3a9d5bfc9cc12b6cf0757dfb6cebb64840eb55c8
Thomas Vandeven <tvand014@uottawa.ca>
2023-09-25 01:11:36 -0400

    Removed the katago override which set "enableGPU =false;".

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
                        echo '${tcd}' >> CMakeLists.txt
                        '';
                    }
-             )).override { enableGPU = false; };
+             ));
 
            lizzieyzy =
              let
